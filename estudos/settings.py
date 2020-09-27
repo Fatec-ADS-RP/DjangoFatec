9f2bdf4a5e3ccc4afe87408bd8aeade88ad2193c
diegobachsi <diegobachsi@gmail.com>
2020-09-27 17:03:26 -0300

    atualizando

--- estudos/settings.py
+++ estudos/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
