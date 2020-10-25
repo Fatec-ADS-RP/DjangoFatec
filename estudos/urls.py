6f5e7f4cfac69ed215fcfb3fae356874b3475bef
diegobachsi <diegobachsi@gmail.com>
2020-10-25 17:27:55 -0300

    atualizando

--- estudos/urls.py
+++ estudos/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('website.urls')),
     path('', include('accounts.urls')),
     path('index/', include('website.urls')),
     path('topicos/', include('topicos.urls')),
 ]
-
-if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
