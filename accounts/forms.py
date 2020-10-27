4c0adc61e1636738a9b2747f2d7c413c8c339bc1
diegobachsi <diegobachsi@gmail.com>
2020-10-27 11:21:04 -0300

    atualizando front

--- accounts/forms.py
+++ accounts/forms.py
@@ -43,3 +43,12 @@ class FormRegister(UserCreationForm):
         self.fields['password1'].widget.attrs.update({'placeholder':'Senha'})        
         self.fields['password2'].widget.attrs.update({'placeholder':'Repetir Senha'})
 
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        error_messages = {
+            'username': {
+                'unique': 'Já existe no cadastro um usuário com este nome.',
+            },
+        }
+
