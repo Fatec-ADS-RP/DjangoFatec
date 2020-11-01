8ec167f728f722991638f5567747097829586654
diegobachsi <diegobachsi@gmail.com>
2020-11-01 19:02:26 -0300

    delete image topicos

--- topicos/models.py
+++ topicos/models.py
@@ -20,12 +20,12 @@ class Topics(models.Model):
 
     start_date = models.DateField('Data de Início', null=True, blank=True)
 
-    image = models.ImageField(
-        upload_to='topicos/images', 
-        verbose_name='Imagem',
-        null=True,
-        blank=True
-    )
+    #image = models.ImageField(
+        #upload_to='topicos/images', 
+        #verbose_name='Imagem',
+        #null=True,
+        #blank=True
+    #)
 
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
 
